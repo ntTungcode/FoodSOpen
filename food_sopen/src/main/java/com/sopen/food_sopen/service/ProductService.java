b6e66ffaa57e2216fd9113846529ea29fd66d3cb
Nguyễn Tiến Tùng <nttung.code@gmail.com>
2023-03-08 17:21:44 +0700

    08.03.2023

--- food_sopen/src/main/java/com/sopen/food_sopen/service/ProductService.java
+++ food_sopen/src/main/java/com/sopen/food_sopen/service/ProductService.java
@@ -1,4 +1,20 @@
 package com.sopen.food_sopen.service;
 
+import com.sopen.food_sopen.dto.request.ProductRequest;
+import com.sopen.food_sopen.entity.Product;
+import com.sopen.food_sopen.repository.ProductRepository;
+
+import java.util.List;
+
 public interface ProductService {
+    Product findProductById(Long id);
+
+    List<Product> getProductsByCategoryId(Long id);
+    Product addNewProduct(ProductRequest productRequest);
+
+    String updateProduct(Long id, ProductRequest productRequest);
+
+    String deleteProduct(Long id);
+
+    List<Product> searchProduct(String searchText);
 }
