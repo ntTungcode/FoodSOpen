6ab383ba22e2313d1ff18e8721580963c45762cf
Nguyễn Tiến Tùng <nttung.code@gmail.com>
2023-03-08 17:32:40 +0700

    08.03.2023

---
+++ food_sopen/src/main/java/com/sopen/food_sopen/dto/response/ProductResponse.java
@@ -0,0 +1,19 @@
+package com.sopen.food_sopen.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+    private Long id;
+    private String productName;
+    private String description;
+    private String image;
+    private Double price;
+}
+
